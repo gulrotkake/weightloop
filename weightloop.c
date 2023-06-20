29d521a5e92b07536be7c122997fa1f969bcb93c
Michael Mortensen <michael@f3k.org>
2023-06-20 18:51:34 +0100

    Pass mac address as arg

--- weightloop.c
+++ weightloop.c
@@ -2,7 +2,10 @@
 #include <gio/gio.h>
 #include <xwiimote.h>
 
-#define MAC_ADDRESS "00_00_00_00_00_00"
+typedef struct Args {
+    GMainLoop *loop;
+    char *mac_address;
+} Args;
 
 static void bluez_signal_adapter_changed(
     GDBusConnection *conn,
@@ -19,6 +22,7 @@ static void bluez_signal_adapter_changed(
     const char *key;
     GVariant *value = NULL;
     const gchar *signature = g_variant_get_type_string(params);
+    Args *args = (Args*)userdata;
 
     if(strcmp(signature, "(sa{sv}as)") != 0) {
         g_print("Invalid signature for %s: %s != %s", signal, signature, "(sa{sv}as)");
@@ -27,7 +31,7 @@ static void bluez_signal_adapter_changed(
 
     g_variant_get(params, "(&sa{sv}as)", &iface, &properties, &unknown);
     while(g_variant_iter_next(properties, "{&sv}", &key, &value)) {
-        if (g_str_has_suffix(path, MAC_ADDRESS) && g_strcmp0(key, "Connected") == 0) {
+        if (g_str_has_suffix(path, args->mac_address) && g_strcmp0(key, "Connected") == 0) {
             if(!g_variant_is_of_type(value, G_VARIANT_TYPE_BOOLEAN)) {
                 g_print("Invalid argument type for %s: %s != %s", key,
                         g_variant_get_type_string(value), "b");
@@ -52,6 +56,11 @@ static void bluez_signal_adapter_changed(
 }
 
 int main(int argc, char **argv) {
+    if (argc != 2) {
+        g_print("Usage: %s <mac address>\n", argv[0]);
+        return 1;
+    }
+
     GDBusConnection *con = g_bus_get_sync(G_BUS_TYPE_SYSTEM, NULL, NULL);
     if (con == NULL) {
         g_print("Not able to get connection to system bus\n");
@@ -59,6 +68,10 @@ int main(int argc, char **argv) {
     }
 
     GMainLoop *loop = g_main_loop_new(NULL, FALSE);
+
+    Args userdata;
+    userdata.loop = loop;
+    userdata.mac_address = *&argv[1];
 
     guint sub = g_dbus_connection_signal_subscribe(con,
                                                    "org.bluez",
@@ -68,7 +81,7 @@ int main(int argc, char **argv) {
                                                    NULL,
                                                    G_DBUS_SIGNAL_FLAGS_NONE,
                                                    bluez_signal_adapter_changed,
-                                                   NULL,
+                                                   &userdata,
                                                    NULL);
 
     g_main_loop_run(loop);

b1e97ace17bb51ad0838ccc5ec5064df4d308687
Michael Mortensen <michael@f3k.org>
2023-06-20 18:39:45 +0100

    Discover balance board

--- weightloop.c
+++ weightloop.c
@@ -2,6 +2,8 @@
 #include <gio/gio.h>
 #include <xwiimote.h>
 
+#define MAC_ADDRESS "00_00_00_00_00_00"
+
 static void bluez_signal_adapter_changed(
     GDBusConnection *conn,
     const gchar *sender,
@@ -11,6 +13,42 @@ static void bluez_signal_adapter_changed(
     GVariant *params,
     void *userdata
 ) {
+    GVariantIter *properties = NULL;
+    GVariantIter *unknown = NULL;
+    const char *iface;
+    const char *key;
+    GVariant *value = NULL;
+    const gchar *signature = g_variant_get_type_string(params);
+
+    if(strcmp(signature, "(sa{sv}as)") != 0) {
+        g_print("Invalid signature for %s: %s != %s", signal, signature, "(sa{sv}as)");
+        goto done;
+    }
+
+    g_variant_get(params, "(&sa{sv}as)", &iface, &properties, &unknown);
+    while(g_variant_iter_next(properties, "{&sv}", &key, &value)) {
+        if (g_str_has_suffix(path, MAC_ADDRESS) && g_strcmp0(key, "Connected") == 0) {
+            if(!g_variant_is_of_type(value, G_VARIANT_TYPE_BOOLEAN)) {
+                g_print("Invalid argument type for %s: %s != %s", key,
+                        g_variant_get_type_string(value), "b");
+                goto done;
+            }
+
+            if (g_variant_get_boolean(value)) {
+                g_print("[up] balance board\n");
+            } else {
+                g_print("[down] balance board\n");
+            }
+        }
+    }
+ done:
+    if(properties != NULL) {
+        g_variant_iter_free(properties);
+    }
+
+    if(value != NULL) {
+        g_variant_unref(value);
+    }
 }
 
 int main(int argc, char **argv) {
